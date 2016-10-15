55ea60c2a8195f1b70c4970621755a9a16a86dd2
tidjungs <waewprach.s@ku.th>
2016-10-15 21:24:46 +0700

    config maze to world

--- core/src/com/mygdx/game/World.java
+++ core/src/com/mygdx/game/World.java
@@ -3,13 +3,19 @@ package com.mygdx.game;
 public class World {
 	private Bomberman bomberman;
     private BombermanGame bombermanGame;
+    private Maze maze;
     
     World(BombermanGame bombermanGame) {
     	this.bombermanGame = bombermanGame;
     	bomberman = new Bomberman(100, 100);
+    	maze = new Maze();
     }
     
     Bomberman getBomberman() {
     	return bomberman;
+    }
+    
+    Maze getMaze() {
+    	return maze;
     }
 }
