f6010b20da971bbf48303f1aa5c696797a826b35
tidjungs <waewprach.s@ku.th>
2016-10-15 19:47:53 +0700

    add GameScreen

---
+++ core/src/com/mygdx/game/GameScreen.java
@@ -0,0 +1,9 @@
+package com.mygdx.game;
+import com.badlogic.gdx.ScreenAdapter;
+
+public class GameScreen extends ScreenAdapter {
+	private BombermanGame bombermanGame;
+	public GameScreen(BombermanGame bombermanGame) {
+		this.bombermanGame = bombermanGame;
+	}
+}
