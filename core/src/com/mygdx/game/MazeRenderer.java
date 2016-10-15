a259456881ba4c2d6b843b99cfae4638db0042bb
tidjungs <waewprach.s@ku.th>
2016-10-15 21:38:02 +0700

    render maze

--- core/src/com/mygdx/game/MazeRenderer.java
+++ core/src/com/mygdx/game/MazeRenderer.java
@@ -1,17 +1,38 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MazeRenderer {
 	private Maze maze;
     private SpriteBatch batch;
+    private Texture wallImage;
 
     public MazeRenderer(SpriteBatch batch, Maze maze) {
         this.maze = maze;
         this.batch = batch;
+        
+        wallImage = new Texture("wall.png");
     }
     
     public void render() {
-    	
+        batch.begin();
+        
+        for(int r=0; r < maze.getHeight(); r++) {
+        	
+        	for(int c=0; c < maze.getWidth(); c++) {
+        		
+        		int x = c * 40;
+        		int y = 600 - (r * 40) - 40;
+        		
+        		if (maze.hasWallAt(r, c)) {
+                    batch.draw(wallImage, x, y);
+        		}
+        		
+        	}
+        	
+        }
+        
+        batch.end();
     }
 }
