581406db0ac6b2fd56bd1605ca80038ed6367c4d
tidjungs <waewprach.s@ku.th>
2016-10-15 20:04:21 +0700

    bomberman run

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -1,21 +1,30 @@
 package com.mygdx.game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class GameScreen extends ScreenAdapter {
 	private BombermanGame bombermanGame;
 	private Texture bombermanImg;
+	private int x;
+	private int y;
 	
 	public GameScreen(BombermanGame bombermanGame) {
 		this.bombermanGame = bombermanGame;
 		bombermanImg = new Texture("bomberman.png");
+		x = 100;
+		y = 100;
 	}
 	
 	public void render (float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		x += 5;
         SpriteBatch batch = bombermanGame.batch;
         batch.begin();
-        batch.draw(bombermanImg, 100, 100);
+        batch.draw(bombermanImg, x, y);
         batch.end();
 	}
 }
