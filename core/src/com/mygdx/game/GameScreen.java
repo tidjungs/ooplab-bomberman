21306809f0075eac189dafffbe3a40fa78081355
tidjungs <waewprach.s@ku.th>
2016-10-15 20:05:47 +0700

    add update method

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -21,10 +21,14 @@ public class GameScreen extends ScreenAdapter {
 	public void render (float delta) {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		x += 5;
+        update(delta);
         SpriteBatch batch = bombermanGame.batch;
         batch.begin();
         batch.draw(bombermanImg, x, y);
         batch.end();
+	}
+	
+	private void update(float delta) {
+		x += 5;
 	}
 }
