9d193b102541a5807a0f690f1c8e31c54c46307c
tidjungs <waewprach.s@ku.th>
2016-10-16 20:52:16 +0700

    add bomb graphic

--- core/src/com/mygdx/game/BombRenderer.java
+++ core/src/com/mygdx/game/BombRenderer.java
@@ -7,16 +7,26 @@ public class BombRenderer {
 	
 	private Bomb bomb;
     private SpriteBatch batch;
+    private Texture bombImage1;
+    private Texture bombImage2;
+    private Texture bombImage3;
+    
     private Texture bombImage;
-
+    
+    private int counter = 0;
 	
 	public BombRenderer(SpriteBatch batch, Bomb bomb) {
 		this.bomb = bomb;
         this.batch = batch;
-        bombImage = new Texture("bomb.png");
+        bombImage1 = new Texture("Bomb_f01.png");
+        bombImage2 = new Texture("Bomb_f02.png");
+        bombImage3 = new Texture("Bomb_f03.png");
+        
+        bombImage = bombImage1;
 	}
 	
 	public void render() {
+		changeBombImage();
 		batch.begin();
 		for(int r=0; r < bomb.getHeight(); r++) {
 			for(int c=0; c < bomb.getWidth(); c++) {
@@ -32,5 +42,19 @@ public class BombRenderer {
 			}     	
 	    }
 		batch.end();
+	}
+	
+	public void changeBombImage() {
+		if(counter == 10) {
+			bombImage = bombImage2;
+		} else if (counter == 20) {
+			bombImage = bombImage3;
+		} else {
+			bombImage = bombImage1;
+		}
+		
+		if(counter++ == 20) {
+			counter = 0;
+		}
 	}
 }
